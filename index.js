dc675d04aaca938c0d5efb1d1704177a61339213
teomac <liquidinobusiness@gmail.com>
2025-09-22 22:57:08 +0200

    Made a more complex http server that responds with a more complex 'website' structure

--- index.js
+++ index.js
@@ -3,13 +3,56 @@ const path = require('path');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/') {
+  /* if (req.url === '/') {
     fs.readFile(path.join(__dirname, 'index.html'), (err, content) => {
       if(err) throw err;
       res.writeHead(200, { 'Content-Type': 'text/html' });
       res.end(content);
     });
-  }
+  } */
+
+    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+    let extension = path.extname(filePath);
+    let contentType = 'text/html';
+    switch(extension) {
+      case '.html' :
+        contentType = 'text/html';
+        break;
+      case '.css' :
+        contentType = 'text/css';
+        break;
+      case '.js' :
+        contentType = 'application/json';
+        break;
+      case '.png' :
+        contentType = 'image/png';
+        break;
+      case '.jpg' :
+        contentType = 'image/jpg';
+        break;
+    }
+
+    fs.readFile(filePath, (err,content) => {
+      if(err) {
+        if(err.code == 'ENOENT') {
+          fs.readFile(path.join(__dirname, 'public', 'error-page.html'), (err, content) => {
+            if (err) throw err;
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(content);
+          })
+        }
+        else {
+          res.writeHead(500);
+          res.end(`Server Error: ${err.code}`);
+        }
+      }
+      else {
+        // Successful response
+        res.writeHead(200, {'Content-Type': contentType});
+        res.end(content, 'utf8');
+      }
+    })
+    console.log(filePath);
 });
 
 const PORT = process.env.PORT || 5100;
